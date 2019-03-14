590278d97b3c2ec04d3ebebd3fae7468e8db5f36
System Administrator <root@MacBook-Pro-de-Infraestructura-23.local>
2019-03-14 14:27:23 -0600

    MI Agenda Digital IDS

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ MVC.controllerInst = new MVC.Controller({
 	model: MVC.Model,
 	view: MVC.View,
 	contentElem: document.querySelector('#contact_form'),
-	endpoint: 'http://localhost:8080/api'
+	endpoint: 'https://agenda-digital-ids.herokuapp.com/api'
 })
